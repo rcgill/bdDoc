cf92dc68033ece127c5b2285aa61346b050d7e5a
Rawld Gill <rgill@altoviso.com>
2012-04-02 17:46:11 -0700

    all infrastructure and global processes in and somewhat working

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 rm -Rf ~/dev/bdDoc/rel
-../dtk/util/buildscripts/build.sh -p standard -p ~/dev/bdDoc/lib/bdDoc -r --releaseDir ~/dev/bdDoc/rel
+../dtk/util/buildscripts/build.sh -p base -p ~/dev/bdDoc/lib/bdDoc -r --releaseDir ~/dev/bdDoc/rel
